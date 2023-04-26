a00f65ce055d887f986e0c702cc9c2b58680b4ed
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-04-26 20:48:28 +0500

    done

--- What Does This Code Do Chapter 6 Part 2 6.26/Program.cs
+++ What Does This Code Do Chapter 6 Part 2 6.26/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class C6Q26
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 5; ++i) 
+        {
+            for (int j = 1; j <= 3; ++j)
+            {
+                for (int k = 1; k <= 4; ++k)
+                {
+                    Console.Write('*');
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
